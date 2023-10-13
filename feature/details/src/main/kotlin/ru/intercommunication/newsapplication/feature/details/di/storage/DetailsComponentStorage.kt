9a1a158a5b5de907899e218be73029bb2a794d11
RobertBik <biktimirov_2003@bk.ru>
2023-10-13 15:03:35 +0500

    fix

--- feature/details/src/main/kotlin/ru/intercommunication/newsapplication/feature/details/di/storage/DetailsComponentStorage.kt
+++ feature/details/src/main/kotlin/ru/intercommunication/newsapplication/feature/details/di/storage/DetailsComponentStorage.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.ViewModel
 import ru.intercommunication.newsapplication.feature.details.di.DaggerDetailsComponent
 import ru.intercommunication.newsapplication.feature.details.di.dependencies.DetailsDependenciesProvider
 
-class DetailsComponentStorage : ViewModel() {
+internal class DetailsComponentStorage : ViewModel() {
     val component =
         DaggerDetailsComponent.factory().create(DetailsDependenciesProvider.dependencies)
 }
